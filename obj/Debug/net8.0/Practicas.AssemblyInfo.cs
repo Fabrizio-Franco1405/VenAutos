462a080c8904f839dbdbfba15ed3f74a489b0a87
Fabrizio <fabrizio.software.dev@gmail.com>
2024-07-09 23:18:58 -0400

    Subiendo el código a Gi-Hub

--- obj/Debug/net8.0/Practicas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Practicas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practicas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992fe7566828800c3929ca2cbf8daa2cde8cd492")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practicas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practicas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
